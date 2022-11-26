8b14060c4f86c1152ee1018b9acf2fb1ce927c2f
Elias Athey <elias@atrium.ai>
2022-11-25 17:05:05 -0800

    Viz census data with streamlit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from snowflake.snowpark.session import Session
 from snowflake.snowpark.functions import avg, sum, col,lit
 from dotenv import load_dotenv
 from os import getenv
+import altair as alt
 
 load_dotenv()
 
@@ -29,26 +30,63 @@ st.write(f'I\'m connected to the {acc} snowflake account.')
 st.write(f'By default, I\'m using the {wh} warehouse, the {role} role,\
   and the {db}.{schema} schema.')
 
-tables = sesh.sql(f'select TABLE_NAME from {db}.information_schema.views where TABLE_SCHEMA = \'DBT\'')
-st.write(f'Here are some tables to use: {tables.collect()}')
+# get data
+data = sesh.table('san_francisco_census')
 
-insecure_sql = st.text_area(f'Please enter some SQL: ')
-print(insecure_sql)
-if len(insecure_sql) > 0:
-  print(insecure_sql)
-  with st.spinner(f'Running sql...'):
-    res = sesh.sql(f'{insecure_sql}').collect()
-    st.dataframe(res)
+# spinners are great for long operations
+with st.spinner(f'Getting raw data...'):
+  st.dataframe(data.collect())
 
+with st.spinner(f'Getting stats...'):
+  df = data.to_pandas()
+  total_pop = df[['ZIP_CODE',
+    'YEAR',
+    'total_population__estimate__total_population__total_population__total']]
+    # 'total_population__marginoferror__total_population__total_population__total']]
+  # st.bar_chart(total_pop, 'ZIP_CODE', ['YEAR', 'total_population__estimate__total_population__total_population__total'])
+  chart = alt.Chart(total_pop).mark_bar().encode(
+    x='YEAR',
+    y=alt.Y('sum(total_population__estimate__total_population__total_population__total)', title='Total Population'),
+    color='YEAR',
+    column='ZIP_CODE'
+  )
+
+  st.altair_chart(chart, use_container_width=False)
+
+
+
+
+
+######## This creates a SQL portal - veyr insecure (allows any SQL commands) ############################
+#########################################################################################################
+# tables = sesh.sql(f'select TABLE_NAME from {db}.information_schema.views where TABLE_SCHEMA = \'DBT\'')
+# st.write(f'Here are some tables to use: {tables.collect()[0]}')
+
+# insecure_sql = st.text_area(f'Please enter some SQL: ')
+# print(insecure_sql)
+# if len(insecure_sql) > 0:
+#   print(insecure_sql)
+#   with st.spinner(f'Running sql...'):
+#     res = sesh.sql(f'{insecure_sql}').collect()
+#     st.dataframe(res)
+#########################################################################################################
+
+
+
+
+
+######## This creates a random data chart from slider input ############################
+########################################################################################
 # st.map(us_addr)
-
 # # input
 # usr_val = st.slider('Slide Me!',1,10)
-
 # # data
 # data = np.random.random([usr_val, 10])
-
 # # viz
 # st.write(f'## Your Value: {usr_val}')
 # st.bar_chart(data)
+########################################################################################
+
+
+
 print('done')
