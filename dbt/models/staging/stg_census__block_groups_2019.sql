13997f494efaaf92a70cda5f6dc825b83824a366
Elias Athey <elias@atrium.ai>
2022-11-26 16:35:56 -0800

    Include more census tables

--- dbt/models/staging/stg_census__block_groups_2019.sql
+++ dbt/models/staging/stg_census__block_groups_2019.sql
@@ -1,2 +1,5 @@
-{% set tables=['2019_CBG_B01','2019_CBG_B02'] %}
+{% set tables=[
+    '2019_CBG_B01',
+    '2019_CBG_B02',
+    '2019_CBG_B03'] %}
 {{ stage_census_table(tables) }}
--- dbt/models/staging/stg_census__block_groups_2020.sql
+++ dbt/models/staging/stg_census__block_groups_2020.sql
@@ -1,2 +1,5 @@
-{% set tables=['2020_CBG_B01'] %}
+{% set tables=[
+    '2020_CBG_B01',
+    '2020_CBG_B02',
+    '2020_CBG_B03'] %}
 {{ stage_census_table(tables) }}
